90710464ae5324127044c1e4b7ef773eac8d5a99
ANOOP GEORGE MUTTATh <anoopgeorge91@outlook.com>
2023-04-10 10:40:06 +0530

    First Commit

---
+++ javaprograms/src/sampleprogram/HelloWorld.java
@@ -0,0 +1,16 @@
+package sampleprogram;
+
+public class HelloWorld 
+{
+	public void display()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args)
+	{
+		HelloWorld obj=new HelloWorld();
+		obj.display();
+	}
+
+}
