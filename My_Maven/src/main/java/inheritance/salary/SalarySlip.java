50fcb47bbb87a53541626600dc7f49a1392ca030
Amrutha Wilson <amruthawilson04@outlook.com>
2023-04-17 14:43:08 +0530

    Third Commit

---
+++ My_Maven/src/main/java/inheritance/salary/SalarySlip.java
@@ -0,0 +1,23 @@
+package inheritance.salary;
+
+public class SalarySlip
+{
+	public void employeeSalary()
+	{
+    float basicPay;
+    float hra;
+    float pf;
+    float deduction;
+    float bonus;
+    float totalSalary;
+    
+    
+    }
+	
+
+	public static void main(String[] args) 
+	{
+		SalarySlip obj=new SalarySlip();
+		obj.employeeSalary();
+	}
+}
